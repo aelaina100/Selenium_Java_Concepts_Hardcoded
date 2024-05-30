cd172654e09782ec50385590293a97ef32e5db11
aelaina100 <aelaina100@gmail.com>
2024-05-30 00:47:00 -0400

    Commit of May 30th,2024: Updating the .java class file of C_SalesForce:==> updating academyTest() method/Test case

---
+++ src/test/java/SeleniumExamplesPkg/Selenium_Concepts_Hardcoded/J_Implicit_ExplicitWaits.java
@@ -0,0 +1,40 @@
+package SeleniumExamplesPkg.Selenium_Concepts_Hardcoded;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class J_Implicit_ExplicitWaits {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Third_Party_Browsers\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		
+		//Global Implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		//Global explicit wait ( Latest syntax) 
+		
+		WebDriverWait d= new WebDriverWait(driver,Duration.ofSeconds(5));
+		d.until(ExpectedConditions.alertIsPresent());   
+		
+		// Never mix up Implicit & Explicit waits together ! Ever !!
+
+		//CRUCIAL: Explore all methods under the 'ExpectedConditions' class. This is of supreme importance.
+			//d.until(ExpectedConditions.
+		
+		
+		
+		
+
+		
+		
+	}
+
+}
